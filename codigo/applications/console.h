27a7c1df502b1340ef94ab7e0ac8e1d916e63727
pablo.brusco <pablo.brusco@c27d9000-822d-24fd-78cf-b1e782edaf7b>
2011-02-12 20:34:02 +0000

    console

--- codigo/applications/console.h
+++ codigo/applications/console.h
@@ -1,7 +1,10 @@
-
-void keyCatcher(int key) ;
+#ifndef __CONSOLE__
+#define __CONSOLE__
+#include "../pantalla/pantalla.h"
+#include "../tipos/tipos_basicos.h"
+void console(int key);
 char decode(int key);
-void run (char[] command);
+void run (command);
 void help();
 void show_all();
 void show_running_tasks();
@@ -11,4 +14,7 @@ void display_merging_task(int id);
 void hide_task(int id);
 void clear_screen();
 void send_error_message();
+char extract_first_word();
+int extract_number();
+#endif
 
