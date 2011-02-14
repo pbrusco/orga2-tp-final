96dc4953d4ea7a2dc0b4802039fc9c4c1efc79ef
marianobianchi08 <marianobianchi08@c27d9000-822d-24fd-78cf-b1e782edaf7b>
2011-02-14 15:15:20 +0000

    Solucionado el tema de problemas de espacio al compilar el kernel

--- codigo/interrupciones/idt.c
+++ codigo/interrupciones/idt.c
@@ -107,11 +107,6 @@ void idtFill() {
 	idt[19].offset_16_31 = (unsigned short) ((unsigned int)(&_isr13) >> 16 & (unsigned int) 0xFFFF);
 
 
-/*	idt[14].offset_0_15 = (unsigned short) ((unsigned int)(&_isrE) & (unsigned int) 0xFFFF); \*/
-/*	idt[14].segsel = (unsigned short) 0x0008; \*/
-/*	idt[14].attr = (unsigned short) 0x8E00; \*/
-/*	idt[14].offset_16_31 = (unsigned short) ((unsigned int)(&_isrE) >> 16 & (unsigned int) 0xFFFF);*/
-
 	idt[32].offset_0_15 = (unsigned short) ((unsigned int)(&_isr20) & (unsigned int) 0xFFFF); \
 	idt[32].segsel = (unsigned short) 0x0008; \
 	idt[32].attr = (unsigned short) 0x8E00; \
