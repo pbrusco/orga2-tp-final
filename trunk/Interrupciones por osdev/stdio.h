cb96c392a987f462bb5b8538869cf0336f2b417a
pablo.brusco <pablo.brusco@c27d9000-822d-24fd-78cf-b1e782edaf7b>
2011-03-12 02:37:54 +0000

    commands

--- trunk/Interrupciones por osdev/stdio.h
+++
@@ -1,8 +0,0 @@
-#ifndef STDIO
-#define STDIO
-
-
-void clear_screen(); //clears the screen, duh
-void print(char *msg, unsigned int line);//prints a message to the screen at line specified
-
-#endif // stdio.h
